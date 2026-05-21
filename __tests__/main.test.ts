919adae3a15765fcbe1f2faac2b5c93399b1c19f
Hiroshi Osano <hiroshi.osano@justincase-tech.com>
2026-05-21 18:14:10 +0900

    feat(deps): support `Node.js v24` runtime

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,7 +7,7 @@ describe('main', () => {
   it.skip('runs', () => {
     process.env.INPUT_MILLISECONDS = '500';
     const np = process.execPath;
-    const ip = path.join(__dirname, '..', 'lib', 'main.js');
+    const ip = path.join(__dirname, '..', 'dist', 'index.js');
     const options: cp.ExecFileSyncOptions = {
       env: process.env,
     };
